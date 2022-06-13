3a383dae0ded804fbb24636aa455ee4f307a39f1
DevSaalih <soliualley@gmail.com>
2022-06-13 21:06:19 +0100

    Update With Content

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -6,13 +6,13 @@ const Hero = () => {
     <div className="mt-20 container mx-auto w-11/12">
       <div className="max-w-3xl">
         <h1 className="text-[#F5F5EB] md:text-7xl text-3xl font-bold">
-          Invest Your Funds With Dollar Asset Classes
+          Make Smart Invesment With Maansa.
         </h1>
       </div>
       <div className="mt-10 pb-32 grid md:grid-cols-2 items-center">
         <div className="">
           <h1 className="text-[#E9EBE0] text-2xl ">
-           Maasa is your automated assets management platform, designed to ensure to your financial security through high-value dollar asset investment 
+           Maasa is your automated assets management platform, designed to ensure to your financial security through high-value dollar asset investment.
           </h1>
           <button className="bg-[#0BB95D] mt-10 rounded-md text-white font-medium px-6 py-2">
           Explore Mannsa Today
